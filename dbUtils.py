9816c3eb74c2877fc62fde392fbe2ce56996c1de
Alexey Suponin <lexxsmith@gmail.com>
2015-08-29 22:29:57 +0300

    added db connection

--- dbUtils.py
+++ dbUtils.py
@@ -1,4 +1,22 @@
-__author__ = 'lexluter1988'
-print "Hello"
-print "something new"
+# Small script to show PostgreSQL and Pyscopg together
+import psycopg2
 
+
+class Dbutils(object):
+
+        def __init__(self, connection=None):
+                self.connection = connection
+
+        def connect(self):
+                try:
+                    connection = psycopg2.connect("dbname='im' user='postgres'")
+                except:
+                    print "I am unable to connect to the database"
+                print "I am connected"
+                return connection
+
+        def test(self):
+                print "Tested"
+
+con = Dbutils()
+con.connect()
