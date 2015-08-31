825d7857972b6fd43f6db4f41b0d356008e91478
Alexey Suponin <lexxsmith@gmail.com>
2015-09-01 00:26:44 +0300

    added db connection

---
+++ myclasses.py
@@ -0,0 +1,66 @@
+class Nets(object):
+    def __init__(self):
+        pass
+
+    def get_net(self):
+        pass
+
+    def change_net(self):
+        pass
+
+
+class Vlan(object):
+    def __init__(self):
+        pass
+
+    def create_vlan(self):
+        pass
+
+    def delete_vlan(self):
+        pass
+
+    def get_vlan(self):
+        pass
+
+    def advertise_vlan(self):
+        pass
+
+
+class Subnet(object):
+    def __init__(self):
+        pass
+
+    def create_subnet(self):
+        pass
+
+    def delete_subnet(self):
+        pass
+
+    def get_subnet(self):
+        pass
+
+    def change_subnet(self):
+        pass
+
+
+class Ve(object):
+    def __init__(self):
+        pass
+
+    def get_ve(self):
+        pass
+
+    def change_ve_address(self):
+        pass
+
+
+class Customer(object):
+    def __init__(self):
+        pass
+
+    def get_customer(self):
+        pass
+
+    def get_customers_ve(self):
+        pass
+
