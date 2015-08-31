444435e78f473c811acaf958d6c41049b4e55fdc
Alexey Suponin <lexxsmith@gmail.com>
2015-08-31 23:21:50 +0300

    added db connection

---
+++ mylogger.py
@@ -0,0 +1,8 @@
+import logging
+
+LOG_FILENAME = 'logging_example.out'
+logging.basicConfig(filename=LOG_FILENAME,
+                    level=logging.DEBUG,
+                    )
+
+logging.debug('This message should go to the log file')
